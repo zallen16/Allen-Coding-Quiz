3e3f8a87b2b4ad90ec15ae97f6b4b18b24833b1b
zallen16 <126983111+zallen16@users.noreply.github.com>
2023-11-26 21:50:34 -0500

    added placeholder question info, started js code

---
+++ script.js
@@ -0,0 +1,53 @@
+generateBtn.addEventListener("click", );
+
+var questions = [
+    {
+        question: "This is going to be question 1",
+        answers: [
+            {text: "Option 1", correct: true},
+            {text: "Option 2", correct: false},
+            {text: "Option 3", correct: false},
+            {text: "Option 4", correct: false},
+        ]
+    },
+    {
+        question: "This is going to be question 2",
+        answers: [
+            {text: "Option 1", correct: true},
+            {text: "Option 2", correct: false},
+            {text: "Option 3", correct: false},
+            {text: "Option 4", correct: false},
+        ]
+    },
+    {
+        question: "This is going to be question 3",
+        answers: [
+            {text: "Option 1", correct: true},
+            {text: "Option 2", correct: false},
+            {text: "Option 3", correct: false},
+            {text: "Option 4", correct: false},
+        ]
+    },
+    {
+        question: "This is going to be question 4",
+        answers: [
+            {text: "Option 1", correct: true},
+            {text: "Option 2", correct: false},
+            {text: "Option 3", correct: false},
+            {text: "Option 4", correct: false},
+        ]
+    },
+    {
+        question: "This is going to be question 5",
+        answers: [
+            {text: "Option 1", correct: true},
+            {text: "Option 2", correct: false},
+            {text: "Option 3", correct: false},
+            {text: "Option 4", correct: false},
+        ]
+    }
+];
+
+var answerOption = document.getElementById("answer-options");
+var startButton = document.getElementById("")
+
